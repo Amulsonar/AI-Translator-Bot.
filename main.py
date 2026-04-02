c08d8c0bf68ec7ca9f1867004573ad386e8f1c7b
Amulsonar <sonaramulbhai4@gmail.com>
2026-04-03 00:21:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import google.generativeai as genai
 from telebot import TeleBot
 
 # Render se Keys uthane ka sahi tarika
-BOT_TOKEN = os.environ.get('8149059077:AAHsh9V-IJz6C60-OAxK1SJQQWsZnz76ndQ')
-GEMINI_KEY = os.environ.get('AIzaSyA1q4yIT7OaBKDKPkuIManewyMnntMlXuE')
+BOT_TOKEN = ('8149059077:AAHsh9V-IJz6C60-OAxK1SJQQWsZnz76ndQ')
+GEMINI_KEY = ('AIzaSyA1q4yIT7OaBKDKPkuIManewyMnntMlXuE')
 
 bot = TeleBot(BOT_TOKEN)
 genai.configure(api_key=GEMINI_KEY)
